a869876e8d7949afbc7aad3d359019227d40dd91
Rosario Chiovaro <rosario.chiovaro@gmail.com>
2020-04-28 22:11:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ import scrape_mars
 sys.setrecursionlimit(2000)
 app = Flask(__name__)
 
-
 client = pymongo.MongoClient()
 db = client.mars_db
 collection = db.mars_facts
-
-
 
 @app.route('/scrape')
 def scrape():
@@ -23,7 +20,6 @@ def home():
     mars = list(db.mars_facts.find())
     print(mars)
     return render_template("index.html", mars = mars)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
